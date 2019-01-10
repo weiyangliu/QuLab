69e1fd310cbed73d24e3db0562b42a835b0853c7
weiyangliu <31689826+weiyangliu@users.noreply.github.com>
2019-01-10 09:50:44 +0800

    Add the comment.

--- qulab/drivers/Anri_SignalGenerator.py
+++ qulab/drivers/Anri_SignalGenerator.py
@@ -7,6 +7,7 @@ from qulab import BaseDriver, QInteger, QOption, QReal, QString, QVector
 class Driver(BaseDriver):
     support_models = ['MG3692C']
     #The instrument should be opened by NI-VISA.
+    #The control language should be SCPI.
     quants = [
         QReal('Frequency', unit='Hz',
           set_cmd=':SOUR:FREQ %(value).13e%(unit)s',
