a7af9f68a493dd7ed9efa88378f3222295c52a32
weiyangliu <31689826+weiyangliu@users.noreply.github.com>
2019-02-21 16:51:43 +0800

    Get Phase

--- qulab/drivers/E5071CENA.py
+++ qulab/drivers/E5071CENA.py
@@ -95,6 +95,34 @@ class Driver(BaseDriver):
         self.setValue('Sweep', 'ON')
         return data
 
+    def get_Phase(self, ch=1, formated=False):
+        '''Get the complex value of S paramenter or formated data'''
+        #Select the measurement
+        self.pna_select(ch)
+        #Stop the sweep
+        self.setValue('Sweep', 'OFF')
+        #Begin a measurement
+        self.write('INIT:IMM')
+        self.write('*WAI')
+        #Get the data
+        #self.write('FORMAT:BORD NORM')
+        self.write('CALC%d:SEL:FORM Phase')
+        if self.model in ['E5071C']:
+            self.write(':FORM:DATA ASC')
+            cmd = ("CALC%d:DATA:FDATA?" % ch) if formated else ("CALC%d:DATA:SDATA?" % ch)
+        else:
+            self.write('FORMAT ASCII')
+            cmd = ("CALC%d:DATA? FDATA" % ch) if formated else ("CALC%d:DATA? SDATA" % ch)
+        data = np.asarray(self.query_ascii_values(cmd))
+        if formated:
+            if self.model in ['E5071C']:
+                data = data[::2]
+        else:
+            data = data[::2]+1j*data[1::2]
+        #Start the sweep
+        self.setValue('Sweep', 'ON')
+        return data
+
     def pna_select(self, ch=1):
         '''Select the measurement'''
         if self.model in ['E5071C']:
